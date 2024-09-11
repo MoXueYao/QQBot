e383ff41a7633b637355718517e80a627a7c07cc
MoXueYao <moxueyao.love@gmail.com>
2024-09-11 16:11:50 +0800

    fix

--- pkg/core/bot/server.py
+++ pkg/core/bot/server.py
@@ -60,12 +60,16 @@ class Listen:
             ):
                 self.close(conn)
                 return
-            # 消息列表去除第一个At消息
-            msgList.pop(0)
-            msg_list_oneMSG = msgList.messages[0]
-            if isinstance(msg_list_oneMSG, Text):
-                if msg_list_oneMSG.text.find(" ") == 0:
-                    msg_list_oneMSG.text = msg_list_oneMSG.text[1:]
+            if only_At:
+                # 消息列表去除第一个At消息
+                msgList.pop(0)
+                msg_list_oneMSG = msgList.messages[0]
+            # 如果消息的第一个字符为空格则去除空格
+            if (
+                isinstance(msg_list_oneMSG, Text)
+                and msg_list_oneMSG.text.find(" ") == 0
+            ):
+                msg_list_oneMSG.text = msg_list_oneMSG.text[1:]
             # 创建事件对象
             event = GroupEvent(
                 Sender(
