1a72e9d75ffa48443c6f526277901f36b900a1fc
MoXueYao <moxueyao.love@gmail.com>
2024-09-13 17:41:13 +0800

    fix

--- pkg/config.py
+++ pkg/config.py
@@ -2,10 +2,10 @@
 loop_interval = 10      # 循环间隔时间(s)
 
 # 权限配置
-super_admin = [1913186696]        # 超级管理员QQ
+super_admin = []        # 超级管理员QQ
 admin = []              # 管理员QQ
 
 # 其他配置
 command_prefix = "/"    # 命令前缀
 only_At = True          # 是否仅允许被@时触发
-Bot_QQ = 1913186696     # 机器人QQ
+Bot_QQ = 1234567890     # 机器人QQ
